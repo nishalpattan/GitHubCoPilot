4f71f5957dc702b71f7e92b52c34f68120e0bd1e
Nishal-Study <nishal-study@Nishals-MacBook-Pro.local>
2025-07-02 17:26:22 -0700

    first copilot code

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# TODO: Implement nth Fibonacci number
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("Input cannot be negative")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
